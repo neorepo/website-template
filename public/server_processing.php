83c49c194702fe1559236fd5276723c8de4df715
jys33 <jys9102@gmail.com>
2020-04-21 21:45:36 -0300

    Update

--- public/server_processing.php
+++ public/server_processing.php
@@ -19,14 +19,15 @@ $last_name = ucwords(strtolower($last_name));
 
 $password = password_hash($password, PASSWORD_DEFAULT);
 
+// Para evitar el error de la funcion json_encode
 $rows = [];
 $last_modified_on = $created_on = date('Y-m-d H:i:s');
+$deleted = 0;
 
 // CRUD
 switch ($action) {
     // CREATE
     case 1:
-        $deleted = 0;
         $q = 'INSERT INTO user (username, first_name, last_name, gender, password, status, created_on, last_modified_on, deleted)
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
         $res = Db::query($q, $username, $first_name, $last_name, $gender, $password, $status, $created_on, $last_modified_on, $deleted);
@@ -44,7 +45,6 @@ switch ($action) {
         break;
     // UPDATE
     case 3:
-        $deleted = 0;
         $q = 'UPDATE user SET first_name = ?, last_name = ?, gender = ?, password = ?, status = ?, last_modified_on = ?, deleted = ? WHERE user_id = ?';
         $res = Db::query($q, $first_name, $last_name, $gender, $password, $status, $last_modified_on, $deleted, $user_id);
         break;
@@ -66,7 +66,7 @@ $message = [
 print json_encode($rows, JSON_UNESCAPED_UNICODE);
 exit;
 
-// String resultante despues de pasar por la función json_encode
+// Ejm: String resultante despues de pasar por la función json_encode
 $message_encode = '{"success":true,"success_message":"<p>Sus datos han sido eliminados exitosamente de la base de datos.<\/p>"}';
 
 /**
